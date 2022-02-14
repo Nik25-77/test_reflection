c974df70d3567b3683d6f3268e264d2cdafcd091
Nik25-77 <Kulman_77@mail.ru>
2022-02-15 03:14:02 +0700

    Initial commit

---
+++ src/main/java/controller/ConsoleHandler.java
@@ -0,0 +1,16 @@
+package controller;
+
+import java.util.Scanner;
+
+public class ConsoleHandler {
+    public void handle(){
+        Scanner scanner= new Scanner(System.in);
+        String betAdd;
+        System.out.println("Введите ставку на риск для вашей ставки");
+        while (true){
+            String[] betData = scanner.nextLine().split(" ");
+        }
+
+
+    }
+}
---
+++ src/main/java/model/Bet.java
@@ -0,0 +1,14 @@
+package model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Bet {
+    private int value;
+    private double risk;
+
+}
